c0796565a24e2f6dc26a6986f4cf916269252066
tkhubert <tkhubert@gmail.com>
2015-04-01 19:53:47 +0100

    Initial Commit

---
+++ OpenCLTutorial/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCLTutorial
+//
+//  Created by Thomas Hubert on 01/04/2015.
+//  Copyright (c) 2015 Thomas Hubert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
